d8939521f142ecc5270d81dc7acecefe23fccecc
Scott Schwab <scottschwab86@gmail.com>
2024-02-02 13:40:12 -0600

    Complete challenge

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,12 @@
 function fibonacci(n) {
   // type your code here
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 if (require.main === module) {
@@ -11,6 +18,16 @@ if (require.main === module) {
 
   console.log("Expecting: 1");
   console.log("=>", fibonacci(2));
+
+  console.log("");
+
+  console.log("Expecting: 2");
+  console.log("=>", fibonacci(3));
+
+  console.log("");
+
+  console.log("Expecting: 0");
+  console.log("=>", fibonacci(0));
 
   console.log("");
 
